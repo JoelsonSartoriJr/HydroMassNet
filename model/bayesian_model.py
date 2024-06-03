dd5821c5eeec05abd85ff79f0bd9e05718808973
Joelson Sartori Junior <joelson@anota.ai>
2024-06-02 22:29:07 -0300

    fix: plot

--- model/bayesian_model.py
+++ model/bayesian_model.py
@@ -1,10 +1,7 @@
 import os
 import sys
-
-# Defina a raiz do projeto
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
-
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -120,6 +117,129 @@ def train_bayesian_density_network(x_train, y_train, x_val, y_val, epochs, batch
 
     return model
 
+def make_predictions_and_plot_residuals(model1, model2, data_val, scaler_y_fit):
+    # Make predictions on validation data
+    for x_data, y_data in data_val:
+        resids1 = y_data[:, 0] - model1(x_data, sampling=False)[:, 0]
+        resids2 = y_data[:, 0] - model2(x_data, sampling=False)[:, 0]
+
+    # Plot residual distributions
+    bins = np.linspace(-2, 2, 100)
+    plt.hist(resids1.numpy(), bins, alpha=0.5, label='No Error Estimation')
+    plt.hist(resids2.numpy(), bins, alpha=0.5, label='Density Network')
+    plt.legend()
+    plt.xlabel('Residuals', fontsize=20)
+    plt.ylabel('Count', fontsize=20)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.savefig('Residuo.png')
+
+def plot_predictive_distributions(model1, model2, data_val, scaler_y_fit):
+    # Sample from predictive distributions
+    for x_data, y_data in data_val:
+        samples1 = model1.samples(x_data, 1000)
+        samples2 = model2.samples(x_data, 1000)
+
+    y_data_real = scaler_y_fit.inverse_transform(y_data)
+
+    # Plot predictive distributions
+    plt.figure(figsize=(20, 10))
+    for i in range(12):
+        plt.subplot(3, 4, i + 1)
+        num = np.random.randint(len(y_data_real), size=1)
+        samples1_real = scaler_y_fit.inverse_transform(samples1[num, :].reshape(1, -1))[0]
+        samples2_real = scaler_y_fit.inverse_transform(samples2[num, :].reshape(1, -1))[0]
+        sns.kdeplot(samples1_real, shade=True, label='BNN')
+        sns.kdeplot(samples2_real, shade=True, label='DBNN')
+        plt.axvline(y_data_real[num], ls=':', color='red', label='True value')
+        plt.title(str(i))
+        plt.gca().get_yaxis().set_ticklabels([])
+        if i < 8:
+            plt.gca().get_xaxis().set_ticklabels([])
+        if i == 3:
+            plt.legend(prop={"size": 12})
+    plt.savefig('pred_bnn.png')
+    plt.show()
+
+def covered(samples, y_true, prc=95.0):
+    """Whether each sample was covered by its predictive interval"""
+    q0 = (100.0 - prc) / 2.0  # lower percentile
+    q1 = 100.0 - q0  # upper percentile
+    within_conf_int = np.zeros(len(y_true))
+    for i in range(len(y_true)):
+        p0 = np.percentile(samples[i, :], q0)
+        p1 = np.percentile(samples[i, :], q1)
+        if p0 <= y_true[i] and p1 > y_true[i]:
+            within_conf_int[i] = 1
+    return within_conf_int
+
+def compute_coverage_and_errors(model1, model2, x_val, y_val, scaler_x_fit, scaler_y_fit):
+    # Sample from predictive distributions
+    samples1 = model1.samples(x_val, 1000)
+    samples2 = model2.samples(x_val, 1000)
+
+    # Compute what samples are covered by their 95% predictive intervals
+    covered1 = covered(samples1, y_val)
+    covered2 = covered(samples2, y_val)
+
+    mean1, mean2 = [], []
+
+    for i in range(len(samples1)):
+        mean1.append(np.mean(samples1[i]))
+        mean2.append(np.mean(samples2[i]))
+
+    print(f'Erro médio absoluto BNN {mean_absolute_error(y_val, mean1):.3f}')
+    print(f'Score R2 BNN: {r2_score(y_val, mean1) * 100:.3f}')
+
+    print(f'Erro médio absoluto DBNN {mean_absolute_error(y_val, mean2):.3f}')
+    print(f'Score R2 DBNN: {r2_score(y_val, mean2) * 100:.3f}')
+
+    mean1 = np.array(mean1).reshape(-1, 1)
+    mean2 = np.array(mean2).reshape(-1, 1)
+    x_val_real = scaler_x_fit.inverse_transform(x_val)
+
+    star_formation = x_val_real[:, -1]
+
+    preds_real = scaler_y_fit.inverse_transform(mean1).flatten()
+    y_val_real = scaler_y_fit.inverse_transform(y_val).flatten()
+
+    plt.figure(figsize=(32, 7))
+
+    error = np.abs(preds_real - y_val_real)  # Ensure no negative values
+    plt.plot(y_val_real, y_val_real, color='green', zorder=3)
+    plt.errorbar(y_val_real, preds_real, yerr=error, fmt="o", alpha=0.7,)
+    plt.scatter(y_val_real, preds_real, c=star_formation, zorder=3, alpha=0.7)
+    # labels
+    plt.xlabel(r'True values ($log(M_{\odot})$)', fontsize=24)
+    plt.ylabel(r'Predict values ($log(M_{\odot})$)', fontsize=24)
+    plt.xticks(fontsize=22)
+    plt.yticks(fontsize=22)
+    # limits
+    plt.xlim(8.5, 10.7)
+    plt.ylim(7, 12)
+    plt.savefig('realVSpreditoBNN.png')
+    plt.show()
+
+    preds_real = scaler_y_fit.inverse_transform(mean2).flatten()
+    y_val_real = scaler_y_fit.inverse_transform(y_val).flatten()
+
+    plt.figure(figsize=(32, 7))
+
+    error = np.abs(preds_real - y_val_real)  # Ensure no negative values
+    plt.plot(y_val_real, y_val_real, color='green', zorder=3)
+    plt.errorbar(y_val_real, preds_real, yerr=error, fmt="o", alpha=0.7,)
+    plt.scatter(y_val_real, preds_real, c=star_formation, zorder=3, alpha=0.7)
+    # labels
+    plt.xlabel(r'True values ($log(M_{\odot})$)', fontsize=24)
+    plt.ylabel(r'Predict values ($log(M_{\odot})$)', fontsize=24)
+    plt.xticks(fontsize=22)
+    plt.yticks(fontsize=22)
+    # limits
+    plt.xlim(8.5, 10.7)
+    plt.ylim(7, 12)
+    plt.savefig('realVSpreditoDBNN.png')
+    plt.show()
+
 if __name__ == "__main__":
     data_path = os.path.join(project_root, 'data', 'cleaning_data_test.csv')
     val_split = 0.2
@@ -133,3 +253,8 @@ if __name__ == "__main__":
 
     bnn_model = train_bayesian_regression(x_train, y_train, x_val, y_val, epochs, batch_size, learning_rate, val_split, "BNN")
     dbnn_model = train_bayesian_density_network(x_train, y_train, x_val, y_val, epochs, batch_size, learning_rate, val_split, "DBNN")
+
+    data_val = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(x_val.shape[0])
+    make_predictions_and_plot_residuals(bnn_model, dbnn_model, data_val, scaler_y_fit)
+    plot_predictive_distributions(bnn_model, dbnn_model, data_val, scaler_y_fit)
+    compute_coverage_and_errors(bnn_model, dbnn_model, x_val, y_val, scaler_x_fit, scaler_y_fit)
