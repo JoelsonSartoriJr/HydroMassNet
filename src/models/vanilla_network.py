2641575df217ba84934a9a3d366e017662936b35
Joelson Sartori Junior <33182102+JoelsonSartoriJr@users.noreply.github.com>
2025-09-08 22:14:20 -0300

    Refactor vanilla model into VanillaNetwork class

--- src/models/vanilla_network.py
+++ src/models/vanilla_network.py
@@ -1,25 +1,37 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Input, Dropout
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Input
 
-def create_vanilla_model(input_shape, layers_config, dropout_rate, learning_rate):
+class VanillaNetwork(tf.keras.Model):
     """
-    Cria um modelo de rede neural densa (vanilla).
+    Construtor para um modelo de rede neural densa simples (Vanilla).
     """
-    inputs = Input(shape=(input_shape,))
-    x = inputs
-    
-    layer_sizes = [int(size) for size in layers_config.split('-')]
-    
-    for size in layer_sizes:
-        x = Dense(size, activation='relu')(x)
-        if dropout_rate > 0:
-            x = Dropout(dropout_rate)(x)
-            
-    outputs = Dense(1)(x)
-    
-    model = Model(inputs=inputs, outputs=outputs)
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
-                  loss='mean_squared_error',
-                  metrics=['mean_absolute_error'])
-    return model
+    def __init__(self, input_shape: tuple, config: dict, name: str = 'vanilla'):
+        super(VanillaNetwork, self).__init__(name=name)
+
+        layer_dims = [int(u) for u in config['layers'].split('-')] + [1]
+        dropout_rate = config.get('dropout', 0.2)
+
+        layers = [Input(shape=input_shape, name="input_layer")]
+        for units in layer_dims[:-1]:
+            layers.extend([Dense(units, activation='relu'), Dropout(dropout_rate)])
+        layers.append(Dense(layer_dims[-1], name="output_layer"))
+
+        self.model = Sequential(layers, name='vanilla_network')
+
+    def call(self, x):
+        """Executa a passagem para a frente (forward pass)."""
+        return self.model(x)
+
+    def compile(self, optimizer):
+        """Compila o modelo com a perda e métricas padrão."""
+        super(VanillaNetwork, self).compile()
+        self.optimizer = optimizer
+        # O modelo sequencial já tem um loop de treino, então só precisamos compilar.
+        self.model.compile(optimizer=self.optimizer, loss='mean_squared_error', metrics=['mae'])
+
+    def train_step(self, data):
+        return self.model.train_step(data)
+
+    def test_step(self, data):
+        return self.model.test_step(data)
