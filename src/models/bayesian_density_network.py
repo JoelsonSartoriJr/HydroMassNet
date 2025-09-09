6604e81943a474ce5159c0c38a32649b2bd58728
Joelson Sartori Junior <33182102+JoelsonSartoriJr@users.noreply.github.com>
2025-09-09 08:35:58 -0300

    Refactor BayesianDenseNetwork with core and head layers

--- src/models/bayesian_density_network.py
+++ src/models/bayesian_density_network.py
@@ -1,61 +1,92 @@
 import tensorflow as tf
-from tensorflow.keras.layers import BatchNormalization, Activation
+import tensorflow_probability as tfp
+from tensorflow.keras.layers import Dense, BatchNormalization, Activation
 from .bayesian_dense_layer import BayesianDenseLayer
 from ..utils.train_utils import parse_layers
 
-class BayesianDenseNetwork(tf.keras.Model):
-    """Rede Neural Densa Bayesiana com lógica de treinamento otimizada."""
+tfd = tfp.distributions
 
-    def __init__(self, n_features: int, layers_config: str, learning_rate: float, name: str = 'bnn'):
-        super(BayesianDenseNetwork, self).__init__(name=name)
+class BayesianDensityNetwork(tf.keras.Model):
+    """Rede Neural de Densidade Bayesiana."""
+
+    def __init__(self, n_features: int, core_layers_config: str, head_layers_config: str, learning_rate: float, name: str = 'dbnn'):
+        super(BayesianDensityNetwork, self).__init__(name=name)
         self.n_features = n_features
-        self.layer_dims = parse_layers(layers_config, n_features, output_dim=1)
-
-        self.bnn_layers = []
-        for i, (d_in, d_out) in enumerate(zip(self.layer_dims[:-1], self.layer_dims[1:])):
-            self.bnn_layers.append(BayesianDenseLayer(d_in, d_out, name=f'{name}_dense_{i}'))
-            if i < len(self.layer_dims) - 2:
-                self.bnn_layers.append(BatchNormalization(name=f'{name}_batch_norm_{i}'))
-                self.bnn_layers.append(Activation('relu', name=f'{name}_activation_{i}'))
-
         self.learning_rate = learning_rate
+        
+        # --- Camadas do Core Bayesiano ---
+        core_dims = parse_layers(core_layers_config, n_features)
+        self.core_layers = []
+        for i, (d_in, d_out) in enumerate(zip(core_dims[:-1], core_dims[1:])):
+            self.core_layers.append(BayesianDenseLayer(d_in, d_out, name=f'{name}_core_dense_{i}'))
+            if i < len(core_dims) - 2:
+                self.core_layers.append(BatchNormalization(name=f'{name}_core_bn_{i}'))
+                self.core_layers.append(Activation('relu', name=f'{name}_core_relu_{i}'))
+
+        # --- Camadas do Head de Densidade (determinístico) ---
+        # A saída do core é a entrada do head
+        head_input_dim = core_dims[-1]
+        # O head prevê os parâmetros da distribuição (média e desvio padrão)
+        head_dims = parse_layers(head_layers_config, head_input_dim, output_dim=2)
+        self.head_layers = []
+        for i, (d_in, d_out) in enumerate(zip(head_dims[:-1], head_dims[1:])):
+            self.head_layers.append(Dense(d_out, name=f'{name}_head_dense_{i}'))
+            if i < len(head_dims) - 2: # Ativação em camadas ocultas do head
+                 self.head_layers.append(Activation('relu', name=f'{name}_head_relu_{i}'))
+
 
     def get_model(self):
         """Constrói e compila o modelo Keras."""
         inputs = tf.keras.Input(shape=(self.n_features,))
-        outputs = self.call(inputs, sampling=True) # Conecta o call para construção
-        model = tf.keras.Model(inputs=inputs, outputs=outputs)
-
-        optimizer = tf.keras.optimizers.Adam(self.learning_rate)
+        distribution = self.call(inputs, sampling=True)
+        # O modelo retorna a distribuição, a perda será calculada sobre ela
+        model = tf.keras.Model(inputs=inputs, outputs=distribution)
         
-        # Otimiza o optimizer para precisão mista, se habilitada
+        optimizer = tf.keras.optimizers.Adam(self.learning_rate)
         if tf.keras.mixed_precision.global_policy().name == 'mixed_float16':
             optimizer = tf.keras.mixed_precision.LossScaleOptimizer(optimizer)
-
-        model.compile(optimizer=optimizer, loss=self.elbo_loss)
+            
+        # A perda é o log-prob da distribuição + KL do core
+        model.compile(optimizer=optimizer, loss=self.dbnn_loss)
         return model
 
     @tf.function
-    def elbo_loss(self, y_true, y_pred):
-        """Negative Evidence Lower Bound (ELBO) loss."""
-        # Garante que os tipos são consistentes para o cálculo da perda
+    def dbnn_loss(self, y_true, y_pred_dist):
+        """Calcula a perda combinada."""
+        # Negativo Log-Probabilidade
         y_true = tf.cast(y_true, tf.float32)
-        y_pred = tf.cast(y_pred, tf.float32)
-        
-        mse_loss = tf.keras.losses.mean_squared_error(y_true, y_pred)
-        kl_loss = sum(self.losses) / self.n_features # Normaliza KL pelo número de features
-        
-        # A loss precisa ser float32 para o LossScaleOptimizer
-        return tf.cast(mse_loss + kl_loss, tf.float32)
+        log_prob = y_pred_dist.log_prob(y_true)
+        nll_loss = -tf.reduce_mean(log_prob)
 
+        # Perda KL das camadas do core
+        kl_loss = sum(self.losses) / self.n_features
+
+        total_loss = nll_loss + kl_loss
+        return tf.cast(total_loss, tf.float32)
+        
     @tf.function
     def call(self, x, sampling=True):
         """Execução forward do modelo."""
-        for layer in self.bnn_layers:
+        # Passa pelo core bayesiano
+        core_output = x
+        for layer in self.core_layers:
             if isinstance(layer, BayesianDenseLayer):
-                x = layer(x, sampling=sampling)
+                core_output = layer(core_output, sampling=sampling)
             elif isinstance(layer, BatchNormalization):
-                x = layer(x, training=sampling)
+                core_output = layer(core_output, training=sampling)
             else:
-                x = layer(x)
-        return x
+                core_output = layer(core_output)
+
+        # Passa pelo head determinístico
+        head_output = core_output
+        for layer in self.head_layers:
+            head_output = layer(head_output)
+            
+        # A saída do head são os parâmetros da distribuição Normal
+        # loc (média), scale (desvio padrão)
+        loc, scale = tf.unstack(head_output, num=2, axis=-1)
+        
+        # Garante que a escala (desvio padrão) seja positiva
+        scale = tf.math.softplus(scale) + 1e-6
+        
+        return tfd.Normal(loc=loc, scale=scale)
