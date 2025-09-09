ca8d4cab7468c354e7d82230f3683effb079e18f
Joelson Sartori Junior <33182102+JoelsonSartoriJr@users.noreply.github.com>
2025-09-09 09:15:55 -0300

    fix

--- src/utils/commands.py
+++ src/utils/commands.py
@@ -1,11 +1,13 @@
 import subprocess
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
 def run_command(command: list):
     """
-    Executa um comando no shell, captura o output e verifica por erros.
+    Executa um comando no shell com o file locking do HDF5 desativado,
+    captura o output e verifica por erros.
 
     Args:
         command (list): Lista de strings representando o comando e seus argumentos.
@@ -19,20 +21,26 @@ def run_command(command: list):
     command_str = ' '.join(command)
     logger.info(f"Executando comando: {command_str}")
 
+    # Cria uma cópia do ambiente atual e adiciona a variável para desativar o lock
+    env = os.environ.copy()
+    env['HDF5_USE_FILE_LOCKING'] = 'FALSE'
+
     try:
+        # Executa o comando com o ambiente modificado
         result = subprocess.run(
             command,
             capture_output=True,
             text=True,
-            check=True,  # Lança exceção automaticamente em caso de erro
-            encoding='utf-8'
+            check=True,
+            encoding='utf-8',
+            env=env  # Passa o ambiente modificado para o subprocesso
         )
         if result.stdout:
             logger.info(f"Comando bem-sucedido. STDOUT:\n{result.stdout.strip()}")
         return result.stdout
 
     except FileNotFoundError:
-        logger.error(f"Erro: Comando '{command[0]}' não encontrado. Verifique se o programa está instalado e no PATH.")
+        logger.error(f"Erro: Comando '{command[0]}' não encontrado.", exc_info=True)
         raise
 
     except subprocess.CalledProcessError as e:
